1936aceaeeca93409e2ec1cfbe9be9503e54d542
Krish <krishguptakg2004@gmail.com>
2025-07-12 18:06:11 +0530

    prac.txt file modified

---
+++ LinkedList/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        DLL obj = new DLL();
+        obj.insertFirst(3);
+        obj.insertFirst(5);
+        obj.insertFirst(9);
+        obj.insertFirst(21);
+        obj.display();
+
+
+
+
+
+
+        
+        
+        /* SLL obj = new SLL();
+        obj.insertFirst(3);
+        obj.insertFirst(5);
+        obj.insertFirst(9);
+        obj.insertFirst(21);
+        obj.insertEnd(30);
+        obj.indexInsert(50,3);
+        obj.display();
+        obj.deleteFirst();
+        obj.display();
+        obj.deleteLast();
+        obj.display();
+     */
+    }
+}
